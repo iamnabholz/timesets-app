1a1bb2d4b545104c0af01e164527042db5f6f07c
Lukas Nabholz <lukas@nabholz.work>
2024-10-06 15:35:14 -0400

    REMOVED EVERYTHING
    (almost everything)

--- src/lib/stores/settings.js
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const theme = writable(localStorage.getItem("theme") || "#CB2A37");
-
-export const currentView = writable(localStorage.getItem("view") || "pomo");
-
-export const hideHour = writable(JSON.parse(localStorage.getItem("hideHour")) || false);
